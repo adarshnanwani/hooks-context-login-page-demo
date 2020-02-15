ede1b631cc5aacb722c744eda8bded69bd3b68ff
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-02-15 13:34:13 +0530

    Language consumer.

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,6 +9,7 @@ import SearchIcon from "@material-ui/icons/Search";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles/NavbarStyles";
 import { ThemeContext } from "./contexts/ThemeContext";
+import { LanguageContext } from "./contexts/LanguageContext";
 
 class Navbar extends Component {
   static contextType = ThemeContext;
@@ -16,34 +17,45 @@ class Navbar extends Component {
     const { isDarkMode, toggleTheme } = this.context;
     const { classes } = this.props;
     return (
-      <div className={classes.root}>
-        <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
-          <Toolbar>
-            <IconButton className={classes.menuButton} color="inherit">
-              <span role="img" aria-label="Spain flag">
-                🇪🇸
-              </span>
-            </IconButton>
-            <Typography className={classes.title} variant="h6" color="inherit">
-              App Title
-            </Typography>
-            <Switch onChange={toggleTheme} />
-            <div className={classes.grow}></div>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search..."
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput
-                }}
-              />
-            </div>
-          </Toolbar>
-        </AppBar>
-      </div>
+      <LanguageContext.Consumer>
+        {({ language }) => (
+          <div className={classes.root}>
+            <AppBar
+              position="static"
+              color={isDarkMode ? "default" : "primary"}
+            >
+              <Toolbar>
+                <IconButton className={classes.menuButton} color="inherit">
+                  <span role="img" aria-label="Spain flag">
+                    🇪🇸 - {language.toUpperCase()}
+                  </span>
+                </IconButton>
+                <Typography
+                  className={classes.title}
+                  variant="h6"
+                  color="inherit"
+                >
+                  App Title
+                </Typography>
+                <Switch onChange={toggleTheme} />
+                <div className={classes.grow}></div>
+                <div className={classes.search}>
+                  <div className={classes.searchIcon}>
+                    <SearchIcon />
+                  </div>
+                  <InputBase
+                    placeholder="Search..."
+                    classes={{
+                      root: classes.inputRoot,
+                      input: classes.inputInput
+                    }}
+                  />
+                </div>
+              </Toolbar>
+            </AppBar>
+          </div>
+        )}
+      </LanguageContext.Consumer>
     );
   }
 }
